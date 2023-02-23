95dae2cd7bb22985115fbfc3f967d1b458adca37
Qual09 <cpx.hieu@gmail.com>
2023-02-23 23:16:32 +0100

    user api

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # nodejs-sandbox
+
+Remember to create .env file in root folder. Sample below:
+
+HOSTNAME=localhost
+PORT=3000
+
+DB_USER=postgres
+DB_PASSWORD=postgres
+DB_HOST=localhost
+DB_PORT=5432
+DB_DATABASE=postgres
+
+TEST=Yo Dawg
--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 
+// Local Modules
+const userRoutes = require('./api/user');
+
 // Environment Viariables
 dotenv.config();
+const apiUrl = '/api';
 
 // App
 const app = express();
@@ -17,15 +21,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(cookieParser());
 
-// Views
+// Root Page
 app.use('/', express.static(__dirname + '/public'));
-app.use('/views', express.static(path.join(__dirname, 'views')));
+
+// Views
+app.use('/server', express.static(path.join(__dirname, 'views')));
+
+// REST API
+app.use(`${apiUrl}/user`, userRoutes);
+app.get(`${apiUrl}/status`, (req, res) => {
+  res.json({ info: 'Node.js, Express, and Postgres API' });
+});
 
 // Start
 const HOSTNAME = process.env.HOSTNAME || 'localhost';
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(process.env.YODAWG);
+  console.log(process.env.TEST);
   console.log(`Server started on http://${HOSTNAME}:${PORT}`);
 });
 
